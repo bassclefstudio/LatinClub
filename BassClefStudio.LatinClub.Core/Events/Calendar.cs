7a52119b4045c498a7b59a50e69fec4af149c2bb
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-10-09 13:32:39 -0400

    Added some basic events classes; edits to appsettings.json.

---
+++ BassClefStudio.LatinClub.Core/Events/Calendar.cs
@@ -0,0 +1,41 @@
+﻿using BassClefStudio.NET.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace BassClefStudio.LatinClub.Core.Events
+{
+    /// <summary>
+    /// Represents a club calendar, with a collection of <see cref="Event"/>s that can be queried.
+    /// </summary>
+    public class Calendar
+    {
+        /// <summary>
+        /// The full collection of <see cref="Event"/>s happening on this <see cref="Calendar"/>.
+        /// </summary>
+        public IEnumerable<Event> Events { get => EventCollection.AsEnumerable(); set { EventCollection.Clear(); EventCollection.AddRange(value); } }
+
+        /// <summary>
+        /// An <see cref="ObservableCollection{T}"/> allowing for dynamic manipulation of the <see cref="Event"/>s in the <see cref="Calendar"/>.
+        /// </summary>
+        [JsonIgnore]
+        public ObservableCollection<Event> EventCollection { get; }
+
+        public Calendar() 
+        {
+            EventCollection = new ObservableCollection<Event>();
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Calendar"/>.
+        /// </summary>
+        /// <param name="events">A collection of <see cref="Event"/>s on the <see cref="Calendar"/>.</param>
+        public Calendar(IEnumerable<Event> events) : this()
+        {
+            Events = events;
+        }
+    }
+}
---
+++ BassClefStudio.LatinClub.Core/Events/Event.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BassClefStudio.LatinClub.Core.Events
+{
+    /// <summary>
+    /// Represents an activity, meeting, or other event that could be present on a calendar.
+    /// </summary>
+    public class Event
+    {
+        /// <summary>
+        /// The time the <see cref="Event"/> begins.
+        /// </summary>
+        public DateTimeOffset StartTime { get; set; }
+
+        /// <summary>
+        /// The name of the <see cref="Event"/>.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Additional information about the <see cref="Event"/>.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The <see cref="EventType"/> describing the function of this <see cref="Event"/>.
+        /// </summary>
+        public EventType Type { get; set; }
+    }
+
+    /// <summary>
+    /// An enum representing the function/category of an <see cref="Event"/>.
+    /// </summary>
+    public enum EventType
+    {
+        /// <summary>
+        /// The <see cref="Event"/> is a meeting of members at a specified time.
+        /// </summary>
+        Meeting = 0,
+        /// <summary>
+        /// The <see cref="Event"/> is a presentation by one or more people or a showing of media.
+        /// </summary>
+        Presentation = 1,
+        /// <summary>
+        /// The <see cref="Event"/> is an activity, game, or competition.
+        /// </summary>
+        Activity = 2,
+        /// <summary>
+        /// The <see cref="Event"/> is the due date of an assignment (i.e. something that needs to be done or submitted).
+        /// </summary>
+        Assignment = 3
+    }
+}
