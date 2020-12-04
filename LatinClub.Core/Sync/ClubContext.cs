f947e8969c8673b37ab6ad901e3a24b1d489fed0
bassclefstudio <bassclefstudio-dev@outlook.com>
2020-12-04 08:12:25 -0500

    Added Sync classes to the .Core project for articles  and events.

---
+++ LatinClub.Core/Sync/ClubContext.cs
@@ -0,0 +1,26 @@
+﻿using BassClefStudio.LatinClub.Core.Events;
+using BassClefStudio.LatinClub.Core.News;
+using BassClefStudio.NET.Core;
+using BassClefStudio.NET.Sync;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassClefStudio.LatinClub.Core.Sync
+{
+    public class ClubContext
+    {
+        public const string ApiUrl = "http://localhost:59525/api";
+
+        public SyncCollection<ClubEvent, int> Events { get; }
+        public SyncCollection<Article, int> Articles { get; }
+
+        public ClubContext()
+        {
+            Events = new EventSyncCollection();
+            Articles = new ArticleSyncCollection();
+        }
+    }
+}
---
+++ LatinClub.Core/Sync/EventLink.cs
@@ -0,0 +1,121 @@
+﻿using BassClefStudio.LatinClub.Core.Events;
+using BassClefStudio.NET.Api;
+using BassClefStudio.NET.Core;
+using BassClefStudio.NET.Sync;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassClefStudio.LatinClub.Core.Sync
+{
+    public class EventLink : ILink<ClubEvent>
+    {
+        public int Id { get; }
+        public EventLink(int id)
+        {
+            Id = id;
+        }
+
+        /// <inheritdoc/>
+        public async Task UpdateAsync(ISyncItem<ClubEvent> item, ISyncInfo<ClubEvent> info = null)
+        {
+            ClubEvent updated = null;
+            if (info is EventSyncCollectionInfo syncInfo)
+            {
+                updated = syncInfo.Events.Get(Id);
+            }
+            else
+            {
+                using (var service = new ApiService())
+                {
+                    updated = await service.GetAsync<ClubEvent>($"{ClubContext.ApiUrl}/events/{Id}");
+                }
+            }
+
+            if (item.Item == null)
+            {
+                item.Item = updated;
+            }
+            else
+            {
+                item.Item.Name = updated.Name;
+                item.Item.StartTime = updated.StartTime;
+                item.Item.Type = updated.Type;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task PushAsync(ISyncItem<ClubEvent> item, ISyncInfo<ClubEvent> info = null)
+        {
+            using (var service = new ApiService())
+            {
+                //service.PatchAsync<Article>($"{ClubContext.ApiUrl}/events/{Id}");
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public class EventSyncItem : KeyedSyncItem<ClubEvent, int>
+    {
+        /// <inheritdoc/>
+        public EventSyncItem(ClubEvent item, ILink<ClubEvent> link = null) : base(item, link)
+        { }
+
+        /// <inheritdoc/>
+        public EventSyncItem(ILink<ClubEvent> link = null) : base(link)
+        { }
+    }
+
+    /// <inheritdoc/>
+    public class EventSyncCollection : SyncCollection<ClubEvent, int>
+    {
+        /// <inheritdoc/>
+        protected override IKeyedSyncItem<ClubEvent, int> CreateSyncItem(ILink<ClubEvent> link)
+        {
+            Debug.WriteLine("Creating SyncItem...");
+            return new EventSyncItem(link);
+        }
+
+        /// <inheritdoc/>
+        protected override async Task<ISyncCollectionInfo<ClubEvent, int>> GetCollectionInfo()
+        {
+            Debug.WriteLine("Getting collection info...");
+            using (var service = new ApiService())
+            {
+                var events = await service.GetAsync<ClubEvent[]>($"{ClubContext.ApiUrl}/events");
+                // Actually include some of the data here at this point.
+                Debug.WriteLine($"Retrieved {events.Length} events in collection info.");
+                return new EventSyncCollectionInfo(events);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override ILink<ClubEvent> GetLink(int key)
+        {
+            return new EventLink(key);
+        }
+    }
+
+    public class EventSyncCollectionInfo : ISyncCollectionInfo<ClubEvent, int>
+    {
+        /// <inheritdoc/>
+        public IEnumerable<int> GetKeys() => Events.Select(a => a.Id);
+
+        /// <summary>
+        /// A collection of returned <see cref="Article"/>s.
+        /// </summary>
+        public IEnumerable<ClubEvent> Events { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="ArticleSyncCollectionInfo"/> from the given keys.
+        /// </summary>
+        /// <param name="events">A collection of <see cref="ClubEvent"/> items in the collection.</param>
+        public EventSyncCollectionInfo(IEnumerable<ClubEvent> events)
+        {
+            Events = events;
+        }
+    }
+}
