5fe2413823977e74fc4a553048a8bf272f5050f3
bassclefstudio <bassclefstudio-dev@outlook.com>
2020-12-04 08:51:49 -0500

    Added AppModel project as well as a Blazor head, which is now attached to the GitHub actions script.

---
+++ LatinClub.Client.Blazor/Pages/BlazorViews.cs
@@ -0,0 +1,14 @@
+﻿using BassClefStudio.AppModel.Navigation;
+using BassClefStudio.LatinClub.Client.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BassClefStudio.LatinClub.Client.Blazor.Pages
+{
+    public class MainBlazorView : BlazorView<MainViewModel>
+    {
+        public override string ViewPath { get; } = "/";
+    }
+}
---
+++ LatinClub.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using BassClefStudio.AppModel.Lifecycle;
+using BassClefStudio.AppModel.Navigation;
+using BassClefStudio.NET.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassClefStudio.LatinClub.Client.ViewModels
+{
+    public class MainViewModel : Observable, IViewModel, IActivationHandler
+    {
+        public bool Enabled { get; } = true;
+
+        public void Activate(IActivatedEventArgs args)
+        { }
+
+        public bool CanHandle(IActivatedEventArgs args)
+        {
+            return args is LaunchActivatedEventArgs;
+        }
+
+        public async Task InitializeAsync()
+        { }
+    }
+}
